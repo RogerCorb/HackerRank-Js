681775cb102dca23d50ddb8739e9f104e569d16b
Rogerio corbo <rogeriocgprest@gmail.com>
2023-07-17 08:36:25 -0300

    melhoria for

--- brasilcomP.js
+++ brasilcomP.js
@@ -10,12 +10,11 @@ function solucao(primeiraLetra, segundaLetra, palavras) {
     let letra1="",letra2="";
     for (let palavra of palavras ) {
           
-          for (let i=0; i<1 ; i++) {           
+          for (let i=0; i<1 ; i++) {   
             
             letra1=palavra.charAt(0); // charAt(0) pega a primeira letra da palavra            
             letra2=palavra.charAt(1); // charAt(1) pega a segunda letra da palavra                   
-          }
-          
+          }        
         
         if (letra1 == primeiraLetra && letra2==segundaLetra) {
               console.log(palavra);
